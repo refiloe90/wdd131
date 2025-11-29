6d66eeb9f20db94d7f615fc70385cfbee5651cd9
refiloe90 <rsegoto@gmail.com>
2025-11-30 01:08:19 +0200

    form

---
+++ scripts/form.js
@@ -0,0 +1,54 @@
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastmodified').textContent = 'Last Modified: ' + new Date(document.lastModified).toLocaleString();
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+window.onload = function() {
+  const productSelect = document.getElementById("product");
+
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id; 
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+  });
+};
+
+let reviewCount = localStorage.getItem("reviewCount");
+
+if (reviewCount === null) {
+  reviewCount = 0;
+}
+
+reviewCount = parseInt(reviewCount) + 1;
+
+localStorage.setItem("reviewCount", reviewCount);
+
+document.getElementById("counter").textContent =
+  "You have submitted " + reviewCount + " review(s).";
